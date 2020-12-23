14b3d71bbe62fe074a21a24e83d8f16fb49903a8
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2020-12-23 09:21:10 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord, mysql.connector
+
+TOKEN=""
+MYSQLPASSWD=""
+DB=None
+
+client=discord.Client()
+
+@client.event
+async def on_message(message):
+    global cursor
+    if message.author==client.user:
+        return
+    print(message.channel.guild)
+
+@client.event
+async def on_ready():
+    print("Logged in as ",client.user.name)
+    print(client.user.id)
+
+secretfile = open("TOKEN","r")
+TOKEN = secretfile.read()
+secretfile.close()
+secretfile = open("MYSQLPASSWD","r")
+MYSQLPASSWD = secretfile.read()
+secretfile.close()
+DB = mysql.connector.connect(
+    host = "localhost",
+    user = "convbot",
+    password = MYSQLPASSWD
+)
+
+cursor = DB.cursor()
+
+client.run(TOKEN)
