37de52a763636d1520d3384f55c68a7523c9a702
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2020-12-23 09:59:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ secretfile.close()
 secretfile = open("MYSQLPASSWD","r")
 MYSQLPASSWD = secretfile.read()
 secretfile.close()
+print(MYSQLPASSWD)
 DB = mysql.connector.connect(
     host = "localhost",
     user = "root",
