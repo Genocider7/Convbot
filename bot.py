0fb044435a7daf9f8daee625a6db075396161132
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2020-12-27 10:13:08 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -407,7 +407,7 @@ async def on_message(message):
                 match_id = mod_user
             else:
                 match_id = mod_role
-            matches = select("moderators", ("id", ), "moderator = \""+match_id+"\" AND server = \""+str(message.channel.guild.id+"\""))
+            matches = select("moderators", ("id", ), "moderator = \""+match_id+"\" AND server = \""+str(message.channel.guild.id)+"\"")
             if len(matches) == 0:
                 await message.channel.send("Nie znaleziono podanej roli/użytkownika w bazie danych")
                 return
